4c81fe28b923e143c6078835e78c939a3cc0c94e
navneet-28 <snavneet20@iitk.ac.in>
2021-09-19 16:21:53 +0530

    everything

---
+++ Readme.md
@@ -0,0 +1,13 @@
+## GSoC 2021 Projects scrapper
+
+This script uses python `requests` , `pandas` , `bs4` libraries.
+
+To install these required modules, just run the following command :
+
+`pip install -r requirements.txt`
+
+YOu may run either the gsoc_scrapper.py file or the GSoC-scrapper.ipynb jupyter notebook.
+
+Let The script run for few minutes(around 2-3)
+
+### Happy scrapping :)
---
+++ gsoc_scrapper.py
@@ -0,0 +1,30 @@
+import requests
+import pandas as pd
+import bs4
+
+base_url = "https://summerofcode.withgoogle.com/api/program/current/project/?page={}&page_size=50"
+
+example = requests.get(base_url.format(1))
+
+pages = int(example.json()["count"]/50) + 1
+
+main_list = []
+main_list.append([" "," "," "," "," "])
+
+for n in range(1,pages+1):
+    res = requests.get(base_url.format(n))
+    jsonRes = res.json()
+    
+    for i in range(0,len(res.json()["results"])):
+        sub_list = []
+        sub_list.append((n-1)*50 + i + 1)
+        sub_list.append(jsonRes["results"][i]["title"])
+        sub_list.append(jsonRes["results"][i]["student"]["display_name"])
+        sub_list.append(jsonRes["results"][i]["organization"]["name"])
+        sub_list.append(jsonRes["results"][i]["organization"]["website_url"])
+        
+        main_list.append(sub_list)
+        
+df = pd.DataFrame(main_list, columns=["S.No","Project Title","Student","Organization","Website URL"])
+
+df.to_csv('gsoc_scrapped.csv',index=False, sep=";")
